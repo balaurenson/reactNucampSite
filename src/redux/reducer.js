65f2a35eec5640f8f7474affbe5d48c7108483ed
Bridget Laurenson <balaurenson@gmail.com>
2021-07-03 09:36:02 -0500

    React week 4: Excercise: React Redux Form Validation

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,15 +1,15 @@
-import { CAMPSITES }  from '../shared/campsites';
+import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS }  from '../shared/comments';
 import { PARTNERS }  from '../shared/partners';
 import { PROMOTIONS }  from '../shared/promotions';
 
-const initialState = {
+export const initialState = {
     campsites: CAMPSITES,
     comments: COMMENTS,
     partners: PARTNERS,
-    promotions: PROMOTIONS;
+    promotions: PROMOTIONS
 };
 
 export const Reducer = (state = initialState, action) => {
     return state;
-}
+};
